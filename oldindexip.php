eab0a656b53dd13cb76098fc3872e79410780bb8
Kylin Schroeder <45126073+Kylin-Schroeder@users.noreply.github.com>
2018-11-27 10:07:45 -0600

    added old files for testing and/or implementation

---
+++ oldindexip.php
@@ -0,0 +1,37 @@
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$file = "iplogs2102.html";
+$file = fopen($file, "a");
+$goose = "<center><p>IP</p>: $ip </center>";
+fwrite($file, $goose);
+fclose($file);
+?>
+
+<!DOCTYPE html5>
+<html>
+<head>
+<link rel="icon" href="favicon.png"> 
+  <meta name="theme-color" content="#000000">
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="twitter:site" content="@iOSHeadquarters">
+  <meta name="twitter:creator" content="@iOSHeadquarters">
+  <meta name="twitter:title" content="iOSHQ">
+  <meta name="twitter:description" content="Developer and Memer">
+  <meta name="twitter:image" content="http://ioshq.cf/images/icon.png ">
+ <title>iOSHQ</title>
+ <meta charset="utf-8"> 
+ <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+ <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=0"> 
+ <style>body{background: url(images/background.jpg); background-size: cover; background-position: center; font-family: "Helvetica Neue", "Helvetica", sans-serif;}img{border-radius: 50px; width:100; height:100px;}.center{position:fixed; width:100%; height:100%; display:table; pointer-events:none;}.card .content{width:80%; max-width:20em; margin:auto; background-color: rgba(255, 255, 255, 0.99); border-radius: 10px; padding: 15px; padding-top: 20px; padding-bottom: 17px;}.card{padding-top: 10vmax}a{text-decoration:none; color:navy;}</style>
+ </head>
+ <meta name="twitter:card" content="summary" />
+<meta name="twitter:site" content="ioshq.cf" />
+<meta name="twitter:title" content="iOSHeadquarters" />
+<meta name="twitter:description" content="Dev, I'm a memer xd" />
+<meta name="twitter:image" content="http://i.imgur.com/oWuQBhl.jpg " />
+ <body> <center class="card"> <div class="content"> 
+ <center class="pic"> <img src="icon.png"/> 
+ </center> <h1>iOSHQ</h1> <div class="desc">I'm iOSHQ, and I have helped on projects and you never knew it ;). I work for JJS Digital, DotsUnlocks, and DevSquad. Site revamp coming soon. </div><br><div><a href="https://twitter.com/iOSHeadquarters ">Twitter</a> | <a href="http://jjs.digital ">JJS Digital</a> | <a href="http://dotsunlocks.com ">DotsUnlocks</a> | <a href="http://joejoe.me ">Joseph Shenton</a> | <a href="https://paypal.com ">PayPal Coming Soon</a></div></div></center>
+ 
+ </body>
+ </html>
